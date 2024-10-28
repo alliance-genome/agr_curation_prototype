e7a918f366942da677aed53471dd1f5ff6a27561
Christian Pich <cmpich@zfin.org>
2024-10-28 16:59:31 +0100

    remove unused imports

--- src/main/java/org/alliancegenome/curation_api/model/entities/GeneOntologyAnnotation.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/GeneOntologyAnnotation.java
@@ -3,7 +3,6 @@ package org.alliancegenome.curation_api.model.entities;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.alliancegenome.curation_api.constants.LinkMLSchemaConstants;
