59229aa3ca81838d498982ea697dc53c193e09d7
Christian Pich <cmpich@zfin.org>
2024-11-07 12:01:44 +0100

    SCRUM-4190 make xenbase and human gaf file load

--- src/main/java/org/alliancegenome/curation_api/jobs/executors/GeneOntologyAnnotationExecutor.java
+++ src/main/java/org/alliancegenome/curation_api/jobs/executors/GeneOntologyAnnotationExecutor.java
@@ -37,12 +37,19 @@ public class GeneOntologyAnnotationExecutor extends LoadFileExecutor {
 
 		String[] tok = url.split("/");
 		String orgAbbrev = tok[tok.length - 1].toUpperCase();
-		String abbr = orgAbbrev.split("\\.")[0];
+		String abbrName = orgAbbrev.split("\\.")[0];
+		String abbr;
+		if (abbrName.contains("HUMAN")) {
+			abbr = "HUMAN";
+		} else if (abbrName.equalsIgnoreCase("xenbase")) {
+			abbr = "XB";
+		} else {
+			abbr = abbrName;
+		}
 		Organization organization = organizationService.getByAbbr(abbr).getEntity();
 
 		// curie, List<GO curie>
 		Map<String, List<String>> uiMap = new HashMap<>();
-		Set<String> orgIDs = new HashSet<>();
 		GZIPInputStream stream = new GZIPInputStream(new FileInputStream(bulkLoadFileHistory.getBulkLoadFile().getLocalFilePath()));
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(stream))) {
 			Stream<String> lines = br.lines();
@@ -51,10 +58,9 @@ public class GeneOntologyAnnotationExecutor extends LoadFileExecutor {
 			lines.filter(s -> !s.startsWith("!") && StringUtils.isNotEmpty(s)).forEach(s -> {
 				String[] token = s.split("\t");
 				String orgID = token[0];
-				orgIDs.add(orgID);
 				String modID = token[1];
 				String goID = token[4];
-				if (abbr.equals(orgID)) {
+				if (abbr.equalsIgnoreCase(orgID) || (abbr.equals("HUMAN") && orgID.equals("RGD"))) {
 					List<String> goIDs = uiMap.computeIfAbsent(modID, list -> new ArrayList<>());
 					goIDs.add(goID);
 				}
@@ -77,7 +83,15 @@ public class GeneOntologyAnnotationExecutor extends LoadFileExecutor {
 			.stream()
 			.map(entry -> entry.getValue().stream().map(goID -> {
 				GeneOntologyAnnotationDTO dto = new GeneOntologyAnnotationDTO();
-				dto.setGeneIdentifier(abbr + ":" + entry.getKey());
+				String prefix = abbr;
+				if (abbr.equalsIgnoreCase("Xenbase")) {
+					prefix = "Xenbase";
+				}
+				if (abbr.equalsIgnoreCase("HUMAN")) {
+					prefix = null;
+				}
+				String geneIdentifier = prefix != null ? prefix + ":" + entry.getKey() : entry.getKey();
+				dto.setGeneIdentifier(geneIdentifier);
 				dto.setGoTermCurie(goID);
 				return dto;
 			}).toList()).flatMap(Collection::stream).toList();
