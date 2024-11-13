158f00ffb7585bae9a7485c5259f0b4a95c7f0a9
Adam Gibson <agibson@mcw.edu>
2024-11-13 15:39:23 -0600

    SCRUM-4513 create biogrid controller

--- src/main/java/org/alliancegenome/curation_api/controllers/crud/BiogridOrcCrudController.java
+++ src/main/java/org/alliancegenome/curation_api/controllers/crud/BiogridOrcCrudController.java
@@ -2,9 +2,11 @@ package org.alliancegenome.curation_api.controllers.crud;
 
 import org.alliancegenome.curation_api.controllers.base.BaseEntityCrudController;
 import org.alliancegenome.curation_api.dao.CrossReferenceDAO;
-import org.alliancegenome.curation_api.interfaces.crud.CrossReferenceCrudInterface;
+import org.alliancegenome.curation_api.interfaces.crud.BiogridOrcCrudInterface;
 import org.alliancegenome.curation_api.jobs.executors.BiogridOrcExecutor;
 import org.alliancegenome.curation_api.model.entities.CrossReference;
+import org.alliancegenome.curation_api.model.ingest.dto.fms.BiogridOrcIngestFmsDTO;
+import org.alliancegenome.curation_api.response.APIResponse;
 import org.alliancegenome.curation_api.services.CrossReferenceService;
 
 import jakarta.annotation.PostConstruct;
@@ -12,7 +14,7 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 
 @RequestScoped
-public class CrossReferenceCrudController extends BaseEntityCrudController<CrossReferenceService, CrossReference, CrossReferenceDAO> implements CrossReferenceCrudInterface {
+public class BiogridOrcCrudController extends BaseEntityCrudController<CrossReferenceService, CrossReference, CrossReferenceDAO> implements BiogridOrcCrudInterface {
 
 	@Inject
 	CrossReferenceService crossReferenceService;
@@ -24,5 +26,10 @@ public class CrossReferenceCrudController extends BaseEntityCrudController<Cross
 	@PostConstruct
 	protected void init() {
 		setService(crossReferenceService);
+	}
+
+	@Override
+	public APIResponse updateBiogridOrc(String dataProvider, BiogridOrcIngestFmsDTO biogridOrcData) {
+		return biogridOrcExecutor.runLoadApi(dataProvider, biogridOrcData.getData());
 	}
 }
