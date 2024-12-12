a26d74682022c5c3bcaff96c2388c4b8ae406745
Olin Blodgett <nilo@nilosplace.net>
2024-12-12 11:18:52 -0500

    update migration

--- src/main/resources/db/migration/v0.38.0.16__add_dependencies.sql
+++ src/main/resources/db/migration/v0.38.0.16__add_dependencies.sql
@@ -5,3 +5,11 @@ CREATE TABLE bulkload_dependencies (
 
 CREATE INDEX bulkload_dependencies_dependencies_index ON bulkload_dependencies USING btree (dependencies_id);
 CREATE INDEX bulkload_dependencies_depends_index ON bulkload_dependencies USING btree (depends_id);
+
+ALTER TABLE bulkload_dependencies
+   ADD CONSTRAINT bulkload_dependencies_dependencies_id_fk
+   FOREIGN KEY (dependencies_id) REFERENCES bulkload(id);
+
+ALTER TABLE bulkload_dependencies
+   ADD CONSTRAINT bulkload_dependencies_depends_id_fk
+   FOREIGN KEY (depends_id) REFERENCES bulkload(id);
