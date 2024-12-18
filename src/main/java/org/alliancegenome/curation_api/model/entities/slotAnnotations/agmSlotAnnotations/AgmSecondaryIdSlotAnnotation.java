37f72f30827fe01481e2bf0e4719296fc5a11830
Christian Pich <cmpich@zfin.org>
2024-12-18 17:50:20 +0100

    SCRUM-4647 fix checkstyle comments

--- src/main/java/org/alliancegenome/curation_api/model/entities/slotAnnotations/agmSlotAnnotations/AgmSecondaryIdSlotAnnotation.java
+++ src/main/java/org/alliancegenome/curation_api/model/entities/slotAnnotations/agmSlotAnnotations/AgmSecondaryIdSlotAnnotation.java
@@ -9,7 +9,6 @@ import lombok.ToString;
 import org.alliancegenome.curation_api.constants.LinkMLSchemaConstants;
 import org.alliancegenome.curation_api.interfaces.AGRCurationSchemaVersion;
 import org.alliancegenome.curation_api.model.entities.AffectedGenomicModel;
-import org.alliancegenome.curation_api.model.entities.Allele;
 import org.alliancegenome.curation_api.model.entities.slotAnnotations.SecondaryIdSlotAnnotation;
 import org.alliancegenome.curation_api.model.entities.slotAnnotations.SlotAnnotation;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
@@ -18,7 +17,7 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = true)
 @ToString(callSuper = true)
-@AGRCurationSchemaVersion(min = "1.4.0", max = LinkMLSchemaConstants.LATEST_RELEASE, dependencies = { SlotAnnotation.class })
+@AGRCurationSchemaVersion(min = "1.4.0", max = LinkMLSchemaConstants.LATEST_RELEASE, dependencies = {SlotAnnotation.class})
 @Schema(name = "AgmSecondaryIdSlotAnnotation", description = "POJO representing an AGM secondary ID slot annotation")
 public class AgmSecondaryIdSlotAnnotation extends SecondaryIdSlotAnnotation {
 
